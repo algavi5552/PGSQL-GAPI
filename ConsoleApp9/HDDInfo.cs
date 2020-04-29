4c538628fac2b3f16e95e8d048339e3e4ab69ec0
algavi5552 <56834981+algavi5552@users.noreply.github.com>
2020-04-29 23:48:39 +0300

    Add files via upload

---
+++ ConsoleApp9/HDDInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp9
+{
+    public class HDDInfo
+    {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        public void PrintInfoDrive()
+        {
+            foreach (DriveInfo d in allDrives)
+            {
+                Console.WriteLine("Drive {0}", d.Name);
+                
+                if (d.IsReady == true && d.TotalSize !=0)
+                {
+                    Console.WriteLine("Available space to current user:{0, 15} Gb", (d.AvailableFreeSpace)/(1024*1024*1024));
+                }
+            }
+        }
+
+
+    }
+}
+  
+
+
+        
+    
